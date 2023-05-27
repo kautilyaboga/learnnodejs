6d89280e2318d522b8b3765986953c41febde9e9
Kautilya Boga <=>
2023-05-27 13:49:29 +0530

    added mongodb schema

---
+++ server/src/models/planets.mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const plantesSchema = new mongoose.Schema({
+  keplerName : {
+    type : String,
+    required : true,
+  },
+
+  // These ones are not rrequried.
+  // koi_insol : {
+  //   type : Number,
+  //   required : true,
+  // },
+  // koi_prad : {
+  //   type : Number,
+  //   required : true,
+  // },
+  // koi_disposition : { 
+  //   type : String,
+  //   required : true
+  // },
+
+})
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app')
 
 const {loadPlanetsData} = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://kautilyab:7gdhlv2HYQgTaCZb@nasa-db.s3mceoa.mongodb.net/nasa?retryWrites=true&w=majority';
+
+
 const server  = http.createServer(app);
 
+mongoose.connection.once('open', ()=>{
+  console.log('MongoDb connection is ready!');
+})
+
+mongoose.connection.on('error', (err)=>{
+  console.log(err)
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT,()=>{
